af729732bf1046900a9e1bad19b1a4b9ef515a9a
dmitrymelnichenko <qube.mel99@gmail.com>
2023-07-01 17:58:44 +0700

    array_copy

---
+++ ArrayCopy.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class ArrayCopy {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int[] big = new int[20];
+
+        for (int i = 0; i < big.length; i++) {
+            big[i] = Integer.parseInt(reader.readLine());
+
+        }
+
+        int[] small1 = Arrays.copyOfRange(big, 0, 10);
+        int[] small2 = Arrays.copyOfRange(big, 10, 20);
+
+        for (int i = 0; i < small1.length; i++) {
+            System.out.println(small1[i]);
+        }
+
+        for (int i = 0; i < small2.length; i++) {
+            System.out.println(small2[i]);
+        }
+    }
+}
