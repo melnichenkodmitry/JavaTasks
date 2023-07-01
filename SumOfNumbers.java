0dbbe39e9dc32f1f7c6ec7aed00a2cf33fb68d0c
dmitrymelnichenko <qube.mel99@gmail.com>
2023-07-01 17:48:06 +0700

    sum_of_numbers

---
+++ SumOfNumbers.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class SumOfNumbers {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int count = 0;
+        for (; true;) {
+            int number = Integer.parseInt(reader.readLine());
+            count += number;
+            if (number == -1) {
+                System.out.println(count);
+                break;
+            }
+        }
+    }
+}
