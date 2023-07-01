979471fe89c7216762ed253aa524c6ceaef8d44c
dmitrymelnichenko <qube.mel99@gmail.com>
2023-07-01 18:16:32 +0700

    robot_battle

---
+++ Robot.java
@@ -0,0 +1,11 @@
+public class Robot extends AbstractRobot {
+    private String name;
+
+    public Robot(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
