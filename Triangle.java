b219e127ef7b63f1662a1476c1e09ba96bcb80e2
dmitrymelnichenko <qube.mel99@gmail.com>
2023-07-01 17:45:31 +0700

    triangle

---
+++ Triangle.java
@@ -0,0 +1,10 @@
+public class Triangle {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            for (int z = 1; z <= i; z++) {
+                System.out.print("8");
+            }
+            System.out.println();
+        }
+    }
+}
